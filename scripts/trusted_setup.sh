a471eb7e584c9612fa8fcea02469186ff714678d
ama31337 <ama31337@protonmail.com>
2021-03-03 02:35:20 +0100

    commit

--- scripts/trusted_setup.sh
+++ scripts/trusted_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # install docker and compose
-sudo apt insatll -y jq
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo service docker restart
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo apt insatll -y jq
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sudo sh get-docker.sh
+#sudo service docker restart
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
 
 # Download and run the trusted setup file
 curl -OL https://raw.githubusercontent.com/oraichain/oraichain-static-files/master/mainnet-static-files/setup.sh && chmod +x ./setup.sh && ./setup.sh
@@ -27,10 +27,11 @@ mkdir -p ~/trusted-bak
 sudo cp {.oraid/config/node_key.json,.oraid/config/priv_validator_key.json} ~/trusted-bak
 
 # download genesis and start node
+cd ~/.oraid/data && find . -not -name '*.json' -delete
 curl -L https://raw.githubusercontent.com/ama31337/oraitips/main/scripts/genesis.json > ~/genesis.json
 sudo cp ~/genesis.json ~/.oraid/config/genesis.json
 # sed -i 's/persistent_peers = ""/persistent_peers = "d549bdd442caadf557672f9b4efefdff80ee28e2@178.128.57.195:26656"/g' ~/.oraid/config/config.toml
-docker-compose restart orai && docker-compose exec -d orai bash -c 'oraivisor start --p2p.pex false --p2p.persistent_peers "d549bdd442caadf557672f9b4efefdff80ee28e2@178.128.57.195:26656"'
+docker-compose restart orai && docker-compose exec -d orai bash -c 'oraivisor start --p2p.pex false --p2p.persistent_peers "d64eedd36edb1751dfcf9310183ca4bd85bdafbd@13.58.238.192"'
 
 echo "done"
 
